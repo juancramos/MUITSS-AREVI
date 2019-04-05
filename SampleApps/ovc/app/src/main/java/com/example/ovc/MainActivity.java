138b3b4b53296bd7afaf79668c8838bd0e1abd43
Juan Carlos Hernandez Ramos <contacto@juancramos.com>
2019-04-05 18:01:13 +0200

    integrate openCV

---
+++ SampleApps/ovc/app/src/main/java/com/example/ovc/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ovc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(OpenCVLoader.initDebug()){
+            Toast.makeText(this, "openCv successfully loaded", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "openCv cannot be loaded", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
