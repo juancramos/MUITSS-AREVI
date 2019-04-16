3ddd6e554e96cb13cdbce08079f26f2a44d4ec58
Juan Carlos Hernandez Ramos <contacto@juancramos.com>
2019-04-16 20:08:34 +0200

    arcoreopencv sample

---
+++ SampleApps/arcoreopencv/app/src/main/java/com/example/arcoreopencv/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.arcoreopencv;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final double MIN_OPENGL_VERSION = 3.0;
+
+    ArFragment arFragment;
+    ModelRenderable lampPostRenderable;
+
+    @Override
+    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ///OpenCV INIT
+        if(OpenCVLoader.initDebug()){
+            Toast.makeText(this, "openCv successfully loaded", Toast.LENGTH_SHORT).show();
+
+            // Sceneform Init and event handler
+            if (!checkIsSupportedDeviceOrFinish(this)) {
+                return;
+            }
+            arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+            ModelRenderable.builder()
+                    .setSource(this, Uri.parse("model.sfb"))
+                    .build()
+                    .thenAccept(renderable -> lampPostRenderable = renderable)
+                    .exceptionally(throwable -> {
+                        Toast toast =
+                                Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER, 0, 0);
+                        toast.show();
+                        return null;
+                    });
+
+            arFragment.setOnTapArPlaneListener(
+                    (HitResult hitresult, Plane plane, MotionEvent motionevent) -> {
+                        if (lampPostRenderable == null){
+                            return;
+                        }
+
+                        Anchor anchor = hitresult.createAnchor();
+                        AnchorNode anchorNode = new AnchorNode(anchor);
+                        anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                        TransformableNode lamp = new TransformableNode(arFragment.getTransformationSystem());
+                        lamp.setParent(anchorNode);
+                        lamp.setRenderable(lampPostRenderable);
+                        lamp.select();
+                    }
+            );
+        }else{
+            Toast.makeText(this, "openCv cannot be loaded", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later");
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        String openGlVersionString =
+                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
+    }
+}
