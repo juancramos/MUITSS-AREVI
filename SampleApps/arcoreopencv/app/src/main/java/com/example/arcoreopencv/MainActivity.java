46d18f45526c1dacf0105bda86e729783fc0ec95
Juan Carlos Hernandez Ramos <contacto@juancramos.com>
2019-04-17 00:27:11 +0200

    Refactor

--- SampleApps/arcoreopencv/app/src/main/java/com/example/arcoreopencv/MainActivity.java
+++ SampleApps/arcoreopencv/app/src/main/java/com/example/arcoreopencv/MainActivity.java
@@ -27,14 +27,15 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final double MIN_OPENGL_VERSION = 3.0;
 
-    ArFragment arFragment;
-    ModelRenderable lampPostRenderable;
+    private ArFragment arFragment;
+    private ModelRenderable lampPostRenderable;
 
     @Override
     @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         ///OpenCV INIT
         if(OpenCVLoader.initDebug()){
             Toast.makeText(this, "openCv successfully loaded", Toast.LENGTH_SHORT).show();
@@ -43,36 +44,8 @@ public class MainActivity extends AppCompatActivity {
             if (!checkIsSupportedDeviceOrFinish(this)) {
                 return;
             }
-            arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
 
-            ModelRenderable.builder()
-                    .setSource(this, Uri.parse("model.sfb"))
-                    .build()
-                    .thenAccept(renderable -> lampPostRenderable = renderable)
-                    .exceptionally(throwable -> {
-                        Toast toast =
-                                Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
-                        toast.setGravity(Gravity.CENTER, 0, 0);
-                        toast.show();
-                        return null;
-                    });
-
-            arFragment.setOnTapArPlaneListener(
-                    (HitResult hitresult, Plane plane, MotionEvent motionevent) -> {
-                        if (lampPostRenderable == null){
-                            return;
-                        }
-
-                        Anchor anchor = hitresult.createAnchor();
-                        AnchorNode anchorNode = new AnchorNode(anchor);
-                        anchorNode.setParent(arFragment.getArSceneView().getScene());
-
-                        TransformableNode lamp = new TransformableNode(arFragment.getTransformationSystem());
-                        lamp.setParent(anchorNode);
-                        lamp.setRenderable(lampPostRenderable);
-                        lamp.select();
-                    }
-            );
+            SetScenformEnvironment();
         }else{
             Toast.makeText(this, "openCv cannot be loaded", Toast.LENGTH_SHORT).show();
         }
@@ -97,5 +70,39 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
         return true;
+    }
+
+    public void SetScenformEnvironment(){
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("model.sfb"))
+                .build()
+                .thenAccept(renderable -> lampPostRenderable = renderable)
+                .exceptionally(throwable -> {
+                    Toast toast =
+                            Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    return null;
+                });
+
+        arFragment.setOnTapArPlaneListener(
+                (HitResult hitresult, Plane plane, MotionEvent motionevent) -> {
+                    if (lampPostRenderable == null){
+                        return;
+                    }
+
+                    Anchor anchor = hitresult.createAnchor();
+                    AnchorNode anchorNode = new AnchorNode(anchor);
+                    anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+                    TransformableNode lamp = new TransformableNode(arFragment.getTransformationSystem());
+                    lamp.setParent(anchorNode);
+                    lamp.setRenderable(lampPostRenderable);
+                    lamp.select();
+                }
+        );
     }
 }
