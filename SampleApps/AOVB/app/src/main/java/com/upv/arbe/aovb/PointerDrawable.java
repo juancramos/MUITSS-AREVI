58df3d1de6965e36dc8aebfbbbaa6a85eb4443d9
Juan Carlos Hernandez Ramos <contacto@juancramos.com>
2019-06-07 21:51:53 +0200

    arcore pointer

---
+++ SampleApps/AOVB/app/src/main/java/com/upv/arbe/aovb/PointerDrawable.java
@@ -0,0 +1,52 @@
+package com.upv.arbe.aovb;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+
+import javax.annotation.Nullable;
+
+public class PointerDrawable extends Drawable {
+    private final Paint paint = new Paint();
+    private boolean enabled;
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public void draw(@NonNull Canvas canvas) {
+        int cx = canvas.getWidth()/2;
+        int cy = canvas.getHeight()/2;
+        if (enabled) {
+            paint.setColor(Color.GREEN);
+            canvas.drawCircle(cx, cy, 10, paint);
+        } else {
+            paint.setColor(Color.GRAY);
+            canvas.drawText("X", cx, cy, paint);
+        }
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.UNKNOWN;
+    }
+}
