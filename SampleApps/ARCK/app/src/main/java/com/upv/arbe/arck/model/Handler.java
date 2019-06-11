6aa9d2e84c2e23bd7c626ba9d64f000cb198698f
Juan Carlos Hernandez Ramos <contacto@juancramos.com>
2019-06-11 22:03:10 +0200

    Animation and video

--- SampleApps/ARCK/app/src/main/java/com/upv/arbe/arck/model/Handler.java
+++ SampleApps/ARCK/app/src/main/java/com/upv/arbe/arck/model/Handler.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AlertDialog;
 import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.animation.ModelAnimator;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
@@ -28,7 +29,35 @@ public class Handler {
         renderable = pRenderable;
     }
 
+    public void togglePauseAndResume(ModelAnimator animator) {
+        if (animator.isPaused()) {
+            animator.resume();
+        } else if (animator.isStarted()) {
+            animator.pause();
+        } else {
+            animator.start();
+        }
+    }
+
+    public void startAnimation(TransformableNode node, ModelRenderable renderable){
+        if(renderable==null || renderable.getAnimationDataCount() == 0) {
+            return;
+        }
+        for(int i = 0;i < renderable.getAnimationDataCount();i++){
+            // AnimationData animationData = renderable.getAnimationData(i);
+            renderable.getAnimationData(i);
+        }
+        ModelAnimator animator = new ModelAnimator(renderable.getAnimationData(0), renderable);
+        animator.start();
+        node.setOnTapListener(
+                (hitTestResult, motionEvent) -> togglePauseAndResume(animator));
+    }
+
     public void addNodeToScene(HitResult hitResult, ArFragment arFragment) {
+        if (renderable == null) {
+            return;
+        }
+
         // Create the Anchor.
         Anchor anchor = hitResult.createAnchor();
         AnchorNode anchorNode = new AnchorNode(anchor);
@@ -36,6 +65,8 @@ public class Handler {
         node.setRenderable(renderable);
         node.setParent(anchorNode);
         node.select();
+
+        startAnimation(node, renderable);
 
         arFragment.getArSceneView().getScene().addChild(anchorNode);
     }
