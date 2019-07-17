458bb311f8c01f1cf1b310c4f4fee03a44bd1d6b
Juan Carlos Hernandez Ramos <contacto@juancramos.com>
2019-07-17 16:18:00 +0200

    Pointer implementation refactor

--- SampleApps/ARCP/app/src/main/java/com/upv/arbe/arcp/logic/ARModule.java
+++ SampleApps/ARCP/app/src/main/java/com/upv/arbe/arcp/logic/ARModule.java
@@ -1,13 +1,7 @@
 package com.upv.arbe.arcp.logic;
 
-import com.google.ar.sceneform.ux.ArFragment;
-
 public class ARModule {
-
-    private ArFragment fragment;
-
-    public ARModule(ArFragment arFragment) {
-        fragment = arFragment;
+    public ARModule() {
 
     }
 }
