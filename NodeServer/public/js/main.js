a7a4975b4616144a32a4a87702e4cca3c11d5cc0
Juan Carlos Hernandez Ramos <contacto@juancramos.com>
2019-07-25 02:41:56 +0200

    RefactorStun

--- NodeServer/public/js/main.js
+++ NodeServer/public/js/main.js
@@ -15,17 +15,17 @@ var pcConfig = {
     {
       urls: 'turn:numb.viagenie.ca',
       credential: 'muazkh',
-      username: 'webrtc@live.com',
+      username: 'webrtc@live.com'
     },
     {
       urls: 'turn:192.158.29.39:3478?transport=udp',
       credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      username: '28224511:1379330808',
+      username: '28224511:1379330808'
     },
     {
       urls: 'turn:192.158.29.39:3478?transport=tcp',
       credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      username: '28224511:1379330808',
+      username: '28224511:1379330808'
     }
   ]
 };
