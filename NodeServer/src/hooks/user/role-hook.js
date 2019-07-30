0866b16c77033829525bc2c1705d75f93e081878
Juan Carlos Hernandez Ramos <contacto@juancramos.com>
2019-07-31 01:25:17 +0200

    code refactor

--- NodeServer/src/hooks/user/role-hook.js
+++ NodeServer/src/hooks/user/role-hook.js
@@ -4,13 +4,9 @@ module.exports = function () { // eslint-disable-line no-unused-vars
       return hook;
     }
 
-    console.log(hook.data);
-
     if (hook.data && hook.data.role) {
         delete hook.data.role;
     }
-
-    console.log(hook.data);
 
     return hook;
   };
