2903e705c2ca8ddc5e8948fdc4c81a8daaee5b30
Juan Carlos Hernandez Ramos <contacto@juancramos.com>
2019-08-09 03:48:48 +0200

    Posting logging user

---
+++ AREVI/app/src/main/java/com/upv/muitss/arevi/logic/web/interfaces/APIService.java
@@ -0,0 +1,22 @@
+package com.upv.muitss.arevi.logic.web.interfaces;
+
+import com.upv.muitss.arevi.entities.User;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+
+    String USER_API_ROUTE = "/user";
+
+    @GET(USER_API_ROUTE)
+    Call<User> getUser();
+
+    @POST(USER_API_ROUTE)
+    @Headers({ "Content-Type: application/json;charset=UTF-8"})
+    Call<User> savePost(@Body User pUser);
+}
+
