398a4903c489b43dc3aa9b9bed7991756c2bb08e
Juan Carlos Hernandez Ramos <contacto@juancramos.com>
2019-08-16 02:39:12 +0200

    Refactor config files

--- arevi-api/sequelize.js
+++ arevi-api/sequelize.js
@@ -1,24 +1,40 @@
 const Sequelize = require('sequelize');
 
 module.exports = function (app) {
-  let connectionString = app.get('mysql');
-  if (process.env.PORT){
+  const connectionString = app.get('mysql');
+  let sequelize = null
+  if (process.env.PORT) {    
+    console.log("pro env");
     const vars = connectionString.split(';');
     const database = vars[0].split('=')[1];
     const source = vars[1].split('=')[1];
+    const hst = source.split(':')[0];
+    const prt = source.split(':')[1];
     const usr = vars[2].split('=')[1];
     const psw = vars[3].split('=')[1];
-    connectionString = `mysql://${usr}:${psw}@${source}/${database}`
+
+    sequelize = new Sequelize(database, usr, psw, {
+      host: hst,
+      port: prt,
+      dialect: 'mysql',
+      logging: false,
+      operatorsAliases: false,
+      define: {
+        freezeTableName: true
+      }
+    })
   }
-  console.log(connectionString);
-  const sequelize = new Sequelize(connectionString, {
-    dialect: 'mysql',
-    logging: false,
-    operatorsAliases: false,
-    define: {
-      freezeTableName: true
-    }
-  });
+  else {
+    sequelize = new Sequelize(connectionString, {
+      dialect: 'mysql',
+      logging: false,
+      operatorsAliases: false,
+      define: {
+        freezeTableName: true
+      }
+    });
+  }
+
   const oldSetup = app.setup;
 
   app.set('sequelizeClient', sequelize);
